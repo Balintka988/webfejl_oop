b56e15841c2535feae63a51bfd9e467b7b6e597c
Szikora Balint <balint.szikora00@gmail.com>
2024-11-29 20:28:06 +0100

    classos szintaxissal

--- basics/script.js
+++ basics/script.js
@@ -1,35 +1,67 @@
-/*
-*5.feladat
-*/
-function Player(nickname){
-    this.nickname = nickname;
-    this.playedMatch = 0;
+///*
+//*5.feladat
+//*/
+//function Player(nickname){
+//    this.nickname = nickname;
+//    this.playedMatch = 0;
+//}
+//
+///*
+//*6.feladat
+//*/
+//Player.prototype.play = function () {
+//    this.playedMatch++;
+//    console.log(this.nickname, this.playedMatch);
+//}
+///*
+//*7.feladat
+//*/
+//Player.prototype.getTierLevel = function(){
+//    if(this.playedMatch <= 3){
+//        return "A";
+//    }
+//    if(this.playedMatch <= 6){
+//        return "B";
+//    }
+//    else{
+//        return "C"
+//    }
+//}
+///*
+//*8.feladat átalakítva
+//*/
+//function PrintTierLevel(player){
+//    console.log(player.nickname, player.getTierLevel());
+//}
+
+class Player{
+    ///*
+
+    constructor(nickname){
+        this.nickname = nickname;
+        this.playedMatch = 0;
+    }
+
+
+    play () {
+        this.playedMatch++;
+        console.log(this.nickname, this.playedMatch);
+    }
+
+    getTierLevel (){
+        if(this.playedMatch <= 3){
+            return "A";
+        }
+        if(this.playedMatch <= 6){
+            return "B";
+        }
+        else{
+            return "C"
+        }
+    }
 }
 
-/*
-*6.feladat
-*/
-Player.prototype.play = function () {
-    this.playedMatch++;
-    console.log(this.nickname, this.playedMatch);
-}
-/*
-*7.feladat
-*/
-Player.prototype.getTierLevel = function(){
-    if(this.playedMatch <= 3){
-        return "A";
-    }
-    if(this.playedMatch <= 6){
-        return "B";
-    }
-    else{
-        return "C"
-    }
-}
-/*
-*8.feladat átalakítva
-*/
+
 function PrintTierLevel(player){
     console.log(player.nickname, player.getTierLevel());
 }
