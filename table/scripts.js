f4175b8e83eeed3a71886b324be675b5d5f1d151
Szikora Balint <szikora.balint@BOLYAI.LOCAL>
2024-12-13 15:01:19 +0100

    tablazat megjelenik

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,42 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+//1.lépés
+class Person{
+    constructor(obj){
+        this.firstname1 = obj.firstname1;
+        this.firstname2 = obj.firstname2;
+        this.lastname = obj.lastname;
+    }
+    render(parentElement){
+        const row = document.createElement('tr');
+        parentElement.appendChild(row);
+
+        const td3 = document.createElement('td');
+        td3.innerHTML = this.lastname;
+        row.appendChild(td3);
+
+        const td1 = document.createElement('td');
+        td1.innerHTML = this.firstname1;
+        row.appendChild(td1);
+
+        if(this.firstname2 !== undefined){
+            const td2 = document.createElement('td');
+            td2.innerHTML = this.firstname2;
+            row.appendChild(td2);
+        }
+        else{
+            td1.colSpan = 2;
+        }
+        
+    }
+}
+
+//2.lépés
+function init(){
+    for(const pers of array){
+        const per = new Person(pers);
+        per.render(document.getElementById('tbodyId'));
+    }
+}
+init();
